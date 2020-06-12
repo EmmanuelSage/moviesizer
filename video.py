30b6b73185f65526930049bf1132b192bbf0ec86
Emmanuel Oluyale <xyemmanuel@gmail.com>
2020-06-12 23:38:10 +0100

    add try catch to getDuration function

--- video.py
+++ video.py
@@ -2,9 +2,16 @@ from moviepy.video.io.VideoFileClip import VideoFileClip
 from pathlib import Path
 
 def getDuration(path):
-    clip = VideoFileClip(path)
-    dur = int(clip.duration)
-    return dur
+    dur = 0
+    try:
+        clip = VideoFileClip(path)
+        dur = int(clip.duration)
+    except:
+        print("File Error ============================")
+        print(path)
+        print("File Error ============================")
+    finally:
+        return dur
 
 def processDuration(dur):
     hrs, mins, secs = dur//60//60, dur//60%60, dur%60
@@ -27,6 +34,7 @@ def getVideosLength():
     for path in pathlist:
         # because path is object not string
         path_in_str = str(path)
+        print(path_in_str)
         duration += getDuration(path_in_str)
         print("Current Duration is : ", duration)
 
@@ -36,6 +44,7 @@ def getVideosLength():
     processDuration(duration)
 
 getVideosLength()
+
 
 # Todo : Add doc strings
 # Todo : Sanitize input
